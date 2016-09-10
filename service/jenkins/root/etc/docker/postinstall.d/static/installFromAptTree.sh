cb91303802c6d07cfe1424786157d5bbc28c6a58
Michael Eichinger <raspidocker@mgeo.de>
2016-09-11 00:49:02 +0200

    Try Jenkins

--- service/jenkins/root/etc/docker/postinstall.d/static/installFromAptTree.sh
+++ service/jenkins/root/etc/docker/postinstall.d/static/installFromAptTree.sh
@@ -4,8 +4,8 @@ jenkins_version=${ENV_JENKINS_VERSION:-"ERROR"}
 d_url=http://mirrors.jenkins-ci.org/war/${jenkins_version}/jenkins.war
 
 if [ ! -e /app ]; then
-    mkdir /app
-    chmod 777 /app
+    mkdir /app/home
+    chmod -R 777 /app
 fi
 
 if [ "$jenkins_version" = "ERROR" ];then
