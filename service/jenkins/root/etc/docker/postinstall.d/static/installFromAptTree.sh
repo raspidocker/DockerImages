41b9f28ce0b45881388abb8bf6737133040f2bc6
Michael Eichinger <raspidocker@mgeo.de>
2016-09-10 03:29:41 +0200

    Jenkis 2.21 war-file

--- service/jenkins/root/etc/docker/postinstall.d/static/installFromAptTree.sh
+++ service/jenkins/root/etc/docker/postinstall.d/static/installFromAptTree.sh
@@ -3,20 +3,21 @@
 jenkins_version=${ENV_JENKINS_VERSION:-"ERROR"}
 d_url=http://mirrors.jenkins-ci.org/war/${jenkins_version}/jenkins.war
 
+if [ ! -e /tomcat/webapps ]; then
+    mkdir /tomcat
+    ln -s /var/lib/tomcat7/webapps /tomcat/webapps
+fi
+cd /tomcat/webapps/
 
 if [ "$jenkins_version" = "ERROR" ];then
     echo "[ERROR] - Jenkinsversion is not set";
     exit 1;
+else
+    echo "* Downloading $d_url"
+    curl -OL $d_url
+
+    echo "* After installing WAR, list webapps-dir:"
+    ls -la /tomcat/webapps/
 fi
-
-mkdir /tomcat
-ln -s /var/lib/tomcat7/webapps /tomcat/webapps
-cd /tomcat/webapps/
-
-echo "* Downloading $d_url"
-curl $d_url
-
-echo "* After installing WAR, list webapps-dir:"
-ls -la /tomcat/webapps/
 
 exit 0;
