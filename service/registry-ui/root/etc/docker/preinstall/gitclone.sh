3b8bd39c69a729bb91c94d70ee6a5e3472fcad18
Michael Eichinger <raspidocker@mgeo.de>
2016-09-12 02:56:17 +0200

    GIT_SERVER ENV

--- service/registry-ui/root/etc/docker/preinstall/gitclone.sh
+++ service/registry-ui/root/etc/docker/preinstall/gitclone.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-WDIR_ROOT=${ENV_WORKING_DIR:="/src"}
-TDIR="${ENV_APPLICATION_DIR:="/app"}"
-PRJ=${ENV_PROJECT_NAME:="docker-registry-ui"}
-REPO=${ENV_GIT_REPOSITORY:="atc-/docker-registry-ui"}
+WDIR_ROOT=${ENV_WORKING_DIR:-"/src"}
+TDIR="${ENV_APPLICATION_DIR:-"/app"}"
+PRJ=${ENV_PROJECT_NAME:-"docker-registry-ui"}
+REPO=${ENV_GIT_REPOSITORY:-"atc-/docker-registry-ui"}
 WDIR="${WDIR_ROOT}/${PRJ}"
+URL=${ENV_GIT_SERVER:-"https://github.com"}
 
 mkdir -p ${WDIR}
 cd ${WDIR}
-git clone ${REPO}
+git clone ${URL}/${REPO}
