065c02646275164b2595c359dd73c657c7cfa2a3
Michael Eichinger <raspidocker@mgeo.de>
2016-09-12 02:48:59 +0200

    git download scirpt

--- service/registry-ui/root/etc/docker/preinstall/gitclone.sh
+++ service/registry-ui/root/etc/docker/preinstall/gitclone.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-WDIR_ROOT=${ENV_WORKDIR:="/src"}
-PRJ=${ENV_PROJECT:="docker-registry-ui"}
+WDIR_ROOT=${ENV_WORKING_DIR:="/src"}
+TDIR="${ENV_APPLICATION_DIR:="/app"}"
+PRJ=${ENV_PROJECT_NAME:="docker-registry-ui"}
 REPO=${ENV_GIT_REPOSITORY:="atc-/docker-registry-ui"}
 WDIR="${WDIR_ROOT}/${PRJ}"
 
